9b405d9e340617d461fb781a5fc5dca10bb0342c
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2024-06-06 19:21:08 +0300

    feat: mark day off

--- src/components/Day/Day.tsx
+++ src/components/Day/Day.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Modal } from '../Modal/Modal';
 import { Agenda } from '../Agenda/Agenda';
 import { useSelector } from 'react-redux';
@@ -6,12 +6,27 @@ import { RootState } from '../../store';
 import { createSelector } from '@reduxjs/toolkit';
 import { ITask } from '../../types';
 import { TaskThumb } from '../TaskThumb/TaskThumb';
+import { useDateIndexes } from '../../hooks';
 
 type DayProps = {
   date: Date,
 };
 export const Day = ({ date }: DayProps) => {
   const isToday = new Date().toDateString() === date.toDateString();
+  const [isDayOff, setIsDayOff] = useState<boolean>(false);
+  const { y, m, d } = useDateIndexes(date);
+
+  const req = '' + y + (m < 9 ? '0' + (m + 1) : m + 1) + (d < 10 ? '0' + d : d);
+  useEffect(() => {
+    fetch(`https://isdayoff.ru/${req}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setIsDayOff(!!result);
+      });
+  }, []);
 
   const [isModal, setIsModal] = useState(false);
   const toggleModal = () => setIsModal(!isModal);
@@ -29,7 +44,11 @@ export const Day = ({ date }: DayProps) => {
   return (
     <>
       <li className="day" onClick={toggleModal}>
-        <p className={`day__date  ${isToday ? 'day__date_today' : ''}`}>{date.getDate()}</p>
+        <p
+          className={`day__date  ${isToday ? 'day__date_today' : ''} ${isDayOff ? 'day__date_off' : ''}`}
+        >
+          {date.getDate()}
+        </p>
         <ul>{renderTaskThumbs}</ul>
       </li>
       {isModal && (
